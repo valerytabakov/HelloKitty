b264b659cc07b13a5531661446873c4bb2e00e25
Valerii Tabakov <tabakov2@gmail.com>
2018-06-13 11:47:25 +0300

    Initial commit

---
+++ app/src/main/java/ua/tabakov/hellokitty/MainActivity.java
@@ -0,0 +1,29 @@
+package ua.tabakov.hellokitty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mHelloTextView;
+    private EditText mNameEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mHelloTextView = (TextView)findViewById(R.id.textView); // помните, я просил запомнить идентификатор?
+        mNameEditText = (EditText) findViewById(R.id.editText);
+    }
+
+    public void onClick(View view) {
+
+        if (mNameEditText.getText().length() == 0) {
+            mHelloTextView.setText("Hello Kitty!");
+        } else {
+            mHelloTextView.setText("Вот я и кликнул по " + mNameEditText.getText() + "у");
+        }
+    }
+}
